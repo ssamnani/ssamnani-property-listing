c0296beda99ef80edd6560c36a2afbc47f6d6ca4
Shehzad Samnani <shehzadhsamnani@gmail.com>
2020-06-27 22:52:28 +0800

    Fixing header

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,11 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
 import "./Header.module.css";
-import { Container } from "@material-ui/core";
 import HeaderTabs from "./HeaderTabs";
 import Breadcrumb from "./Breadcrumb";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
 function Header() {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <div>
       <div className="appHeader">
         <div className="navWrapper">
           <img
