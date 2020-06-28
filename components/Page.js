9eae318efe9c046cf0a230b5fb3d0e5e1056713e
ssamnani <shehzadhsamnani@gmail.com>
2020-06-28 21:55:27 +0800

    Updated SearchResult and added icons

--- components/Page.js
+++ components/Page.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
 import React from "react";
-// import { useDispatch } from 'react-redux'
-// import useInterval from '../lib/useInterval'
 import Header from "./Header";
 import Footer from "./Footer";
 import SearchBox from "./SearchBox";
@@ -9,36 +7,34 @@ import SearchResult from "./SearchResult";
 import MortgageCalculator from "./MortgageCalculator";
 import styles from "../styles/Page.module.css";
 import SearchBar from "./SearchBar";
-import "antd/dist/antd.css";
 import Pagination from "./Pagination";
+import "antd/dist/antd.css";
 
 function Page() {
-  // const dispatch = useDispatch()
-
-  // // Tick the time every second
-  // useInterval(() => {
-  //     dispatch({
-  //         type: 'TICK',
-  //         light: true,
-  //         lastUpdate: Date.now(),
-  //     })
-  // }, 1000)
-
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+
+          <link rel="stylesheet" type="text/css" charSet="UTF-8"
+                href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"/>
+          <link rel="stylesheet" type="text/css"
+                href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"/>
       </Head>
 
-      <Header></Header>
+      <Header />
 
       <main>
         <SearchBar />
         <div className="subBodyWrapper">
           <div className="searchResultsWrapper">
             <SearchResult />
-            <Pagination pageStart="1" pageEnd="20" totalProperties="210939" />
+            <Pagination
+                pageStart='1'
+                pageEnd='20'
+                totalProperties='210939'
+            />
             <MortgageCalculator />
           </div>
         </div>
--- components/SearchResult.js
+++ components/SearchResult.js
@@ -1,45 +1,126 @@
-import React from "react";
-import styles from "../styles/SearchResult.module.css";
+import React from 'react'
+import { Card, Avatar } from 'antd'
+import { LeftOutlined, RightOutlined } from '@ant-design/icons'
+import { FaBath } from 'react-icons/fa'
+import { IoIosBed } from 'react-icons/io'
+import { RiCarLine } from 'react-icons/ri'
+import { AiOutlineStar } from 'react-icons/ai'
+import { FiPhone } from 'react-icons/fi'
+import Slider from 'react-slick'
+import '../styles/SearchResult.module.css'
 
 function SearchResult() {
-  return (
-    <div className="propertyCard">
-      <div className="cardHeader">
-        <div className="headerLeft">
-          <div className="agentImage">
-            <img
-              src="https://pictures-my.ippstatic.com/realtors/images/agent/c2d903f03b17447aa33fddd6a7d88285.jpg"
-              alt="Agent Image"
-            />
-          </div>
-          <div className="agentName">Grace Koo</div>
-          <div className="postedOn">Posted today 12:38 AM</div>
+    const { Meta } = Card;
+    // const SampleNextArrow = (props) => {
+    //     const { className, style, onClick } = props;
+    //     return (
+    //         <div
+    //             className={className}
+    //             onClick={onClick}
+    //             style={{ display: "block" }}
+    //         >
+    //             <RightOutlined />
+    //         </div>
+    //     );
+    // }
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // nextArrow: <SampleNextArrow />,
+        // prevArrow: <RightOutlined />
+    };
+
+    return (
+        <div className='search-list'>
+            <ul className='search-list-ul'>
+                <li className='search-list-li'>
+                    <Card
+                        title={
+                            <React.Fragment>
+                                <Meta
+                                    avatar={
+                                        <Avatar
+                                            size={64}
+                                            src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
+                                            style={
+                                                {
+                                                    border: '1px solid rgb(210, 214, 218)',
+                                                    position: 'absolute',
+                                                    backgroundColor: 'white',
+                                                    zIndex: 10,
+                                                }
+                                            }
+                                        />
+                                    }
+                                    title={
+                                        <React.Fragment>
+                                            <span className='card-title'>
+                                                Shervy Liew
+                                            </span>
+                                            <span className='card-description'>
+                                                Posted today 11:30 PM
+                                            </span>
+                                        </React.Fragment>
+                                    }
+                                />
+                            </React.Fragment>
+                        }
+                        extra={
+                            <img
+                                className='card-extra'
+                                src="https://images-my.ippstatic.com/images/searchresult/agencybrandlogo/0771059589124aa19fd8daf5c60c8dc8.png"
+                                alt="SMY Valuers And Consultants"
+                                data-sizes="auto"
+                                sizes="43px"
+                            />
+                        }
+                        loading={false}
+                        style={{ width: 750 }}
+                    >
+                        <Slider {...settings}>
+                            <div className="carouselWrapper">
+                                <img
+                                    className="carouselImage"
+                                    src="https://img.rea-asia.com/my-subsale/premium/750x560-fit/realtors/images/640/35327/c1e81b7953ef4a63a8143f6d66074ade.jpg"
+                                    alt="1"
+                                />
+                            </div>
+                            <div className="carouselWrapper">
+                                <img
+                                    className="carouselImage"
+                                    src="https://img.rea-asia.com/my-subsale/premium/750x560-fit/realtors/images/640/35327/c1e81b7953ef4a63a8143f6d66074ade.jpg"
+                                    alt="2"
+                                />
+                            </div>
+                            <div className="carouselWrapper">
+                                <img
+                                    className="carouselImage"
+                                    src="https://img.rea-asia.com/my-subsale/premium/750x560-fit/realtors/images/640/35327/c1e81b7953ef4a63a8143f6d66074ade.jpg"
+                                    alt="3"
+                                />
+                            </div>
+                        </Slider>
+
+                        <div className="descriptionWrapper">
+                            <div className="price">RM 330,000</div>
+                            <div className="place">Aspire Residence, Cyberjaya</div>
+                            <div className="city">
+                                Block 18, Cyberjaya, Cyberjaya, 47130, Selangor
+                            </div>
+                            <div className="specs">
+                                2-sty Terrace/Link House • Built-up : 3,032 sq. ft.
+                            </div>
+                            <div className="cardFooter">
+                            </div>
+                        </div>
+                    </Card>
+                </li>
+            </ul>
         </div>
-        <div className="headerRight">
-          <div className="agencyLogo">
-            <img
-              src="https://images-my.ippstatic.com/images/searchresult/agencybrandlogo/0771059589124aa19fd8daf5c60c8dc8.png"
-              alt="Agency Logo"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="carouselWrapper">
-        <img src="https://img.rea-asia.com/my-subsale/premium/750x560-fit/realtors/images/640/35327/c1e81b7953ef4a63a8143f6d66074ade.jpg" />
-      </div>
-      <div className="descriptionWrapper">
-        <div className="price">RM 330,000</div>
-        <div className="place">Aspire Residence, Cyberjaya</div>
-        <div className="city">
-          Block 18, Cyberjaya, Cyberjaya, 47130, Selangor
-        </div>
-        <div className="specs">
-          2-sty Terrace/Link House • Built-up : 3,032 sq. ft.
-        </div>
-        <div className="cardFooter"></div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default SearchResult;
+export default SearchResult
