10b39f5c88ff13cce8de59642ca25b8b76bb43b6
Shehzad Samnani <shehzadhsamnani@gmail.com>
2020-07-01 12:49:30 +0800

    Code refactored

--- components/SearchResult.js
+++ components/SearchResult.js
@@ -60,30 +60,30 @@ function SearchResult() {
     return mapping;
   };
 
-  let localStorage = {};
-  const getSavedPropertyIds = () => {
-    const savedProperties =
-      JSON.parse(localStorage.getItem("savedProperties")) || {};
-    return savedProperties;
-  };
-
   let saveProperty = (propertyId) => {
-    const savedProperties =
-      JSON.parse(localStorage.getItem("savedProperties")) || {};
+    const savedProperties = window
+      ? JSON.parse(window.localStorage.getItem("savedProperties")) || {}
+      : {};
     if (savedProperties[propertyId]) {
       delete savedProperties[propertyId];
     } else {
       savedProperties[propertyId] = true;
     }
     setSavedPropertyIds(savedProperties);
-    localStorage.setItem("savedProperties", JSON.stringify(savedProperties));
+    window &&
+      window.localStorage.setItem(
+        "savedProperties",
+        JSON.stringify(savedProperties)
+      );
   };
 
   useEffect(() => {
-    localStorage = window.localStorage;
-  });
+    const getSavedPropertyIds = () => {
+      const savedProperties =
+        JSON.parse(window.localStorage.getItem("savedProperties")) || {};
+      return savedProperties;
+    };
 
-  useEffect(() => {
     setSavedPropertyIds(getSavedPropertyIds());
   }, []);
 
@@ -143,170 +143,181 @@ function SearchResult() {
           const { currency, min: minPrice } = prices[0];
 
           return (
-            <li key={index} className="search-list-li">
-              <Card
-                title={
-                  <React.Fragment>
-                    <Meta
-                      avatar={
-                        <Avatar
-                          size={64}
-                          src={isEmpty(agentImage) ? null : agentImage}
-                          icon={isEmpty(agentImage) ? <UserOutlined /> : null}
-                          style={{
-                            border: "1px solid rgb(210, 214, 218)",
-                            position: "absolute",
-                            backgroundColor: isEmpty(agentImage)
-                              ? "#236a9a"
-                              : "white",
-                            zIndex: 10,
-                          }}
+            <React.Fragment>
+              {index < 20 && (
+                <li key={index} className="search-list-li">
+                  <Card
+                    title={
+                      <React.Fragment>
+                        <Meta
+                          avatar={
+                            <Avatar
+                              size={64}
+                              src={isEmpty(agentImage) ? null : agentImage}
+                              icon={
+                                isEmpty(agentImage) ? <UserOutlined /> : null
+                              }
+                              style={{
+                                border: "1px solid rgb(210, 214, 218)",
+                                position: "absolute",
+                                backgroundColor: isEmpty(agentImage)
+                                  ? "#236a9a"
+                                  : "white",
+                                zIndex: 10,
+                              }}
+                            />
+                          }
+                          title={
+                            <React.Fragment>
+                              <span className="card-title">{agentName}</span>
+                              <span className="card-description">
+                                Posted{" "}
+                                {new Date()
+                                  .toDateString()
+                                  .split(" ")
+                                  .filter((a, i) => i !== 0)
+                                  .join(" ")}
+                              </span>
+                            </React.Fragment>
+                          }
                         />
-                      }
-                      title={
-                        <React.Fragment>
-                          <span className="card-title">{agentName}</span>
-                          <span className="card-description">
-                            Posted{" "}
-                            {new Date()
-                              .toDateString()
-                              .split(" ")
-                              .filter((a, i) => i !== 0)
-                              .join(" ")}
-                          </span>
-                        </React.Fragment>
-                      }
-                    />
-                  </React.Fragment>
-                }
-                extra={
-                  <img
-                    className="card-extra"
-                    src={orgLogoUrl}
-                    alt=""
-                    data-sizes="auto"
-                    sizes="43px"
-                  />
-                }
-                loading={false}
-                style={{
-                  width: 750,
-                  border: "1px solid rgb(210, 214, 218)",
-                  borderRadius: "4px",
-                }}
-                className="search-result-card"
-              >
-                <Slider {...settings}>
-                  {!isEmpty(coverUrl) && (
-                    <div className="carouselWrapper">
+                      </React.Fragment>
+                    }
+                    extra={
                       <img
-                        className="carouselImage"
-                        src={coverUrl}
-                        alt="coverImage"
+                        className="card-extra"
+                        src={orgLogoUrl}
+                        alt=""
+                        data-sizes="auto"
+                        sizes="43px"
                       />
-                    </div>
-                  )}
-                  {!isEmpty(medias) &&
-                    medias.map((item, index) => {
-                      const { url: imageUrl = "" } = item;
-                      return (
-                        <div key={index} className="carouselWrapper">
+                    }
+                    loading={false}
+                    style={{
+                      width: 750,
+                      border: "1px solid rgb(210, 214, 218)",
+                      borderRadius: "4px",
+                    }}
+                    className="search-result-card"
+                  >
+                    <Slider {...settings}>
+                      {!isEmpty(coverUrl) && (
+                        <div className="carouselWrapper">
                           <img
                             className="carouselImage"
-                            src={imageUrl}
-                            alt={"image-" + index}
+                            src={coverUrl}
+                            alt="coverImage"
                           />
                         </div>
-                      );
-                    })}
-                </Slider>
-
-                <div className="descriptionWrapper">
-                  <div className="price">
-                    {minPrice === 0
-                      ? "Contact for price"
-                      : currencyMapper(currency) + " " + minPrice}
-                  </div>
-                  <div className="place">{title}</div>
-                  {formattedAddress && (
-                    <div className="city">{formattedAddress}</div>
-                  )}
-                  <div className="specs">
-                    <ul>
-                      <li>
-                        {propertyType}
-                        {unitType && ` | ${unitType} `}
-                      </li>
-                      {landArea && builtUp === "" && (
-                        <li>Land Area : ${landArea} </li>
                       )}
-                      <li>
-                        {builtUp && `Built-up : ${builtUp} `}
-                        <span>{getPropertySizeUnitMapping("SQUARE_FEET")}</span>
-                      </li>
-                      {furnishing && <li>{`${furnishing}`}</li>}
-                    </ul>
-                  </div>
-                  <div className="cardFooter">
-                    <div className="listing-facilities">
-                      <ul className="attributes-facilities-wrapper">
-                        <li className="attributes-facilities-item-wrapper">
-                          <IoIosBed className="bedroom-facility" />
-                          {bedroom}
-                        </li>
-                        <li className="attributes-facilities-item-wrapper">
-                          <FaBath className="bathroom-facility" />
-                          {bathroom}
-                        </li>
-                        <li className="attributes-facilities-item-wrapper">
-                          <RiCarLine className="carPark-facility" />
-                          {carPark}
-                        </li>
-                      </ul>
-                    </div>
-                    <div className="save-listing-button">
-                      <button
-                        id="saved-listing-wrapper"
-                        onClick={() => saveProperty(item.id)}
-                      >
-                        {savedPropertyIds[item.id] ? (
-                          <AiFillStar className="save-icon" />
-                        ) : (
-                          <AiOutlineStar
-                            className="save-icon"
-                            id="saved-listing-icon"
-                          />
-                        )}
-                        <span className="save-text">Save</span>
-                      </button>
-                    </div>
-                    <div className="contact-detail-button">
-                      <div className="contact-detail-button-first">
-                        <a className="phone-desktop">
-                          <FiPhone className="phone-icon" />
-                          <span className="phone-number">+60162...</span>
-                        </a>
-                        <a className="phone-mobile" href={"tel:" + agentPhone}>
-                          <FiPhone className="phone-icon" />
-                          <span>Call agent</span>
-                        </a>
+                      {!isEmpty(medias) &&
+                        medias.map((item, index) => {
+                          const { url: imageUrl = "" } = item;
+                          return (
+                            <div key={index} className="carouselWrapper">
+                              <img
+                                className="carouselImage"
+                                src={imageUrl}
+                                alt={"image-" + index}
+                              />
+                            </div>
+                          );
+                        })}
+                    </Slider>
+
+                    <div className="descriptionWrapper">
+                      <div className="price">
+                        {minPrice === 0
+                          ? "Contact for price"
+                          : currencyMapper(currency) + " " + minPrice}
                       </div>
-                      <div className="contact-detail-button-second">
-                        <a
-                          className="depth-listing-card-link"
-                          href="https://www.iproperty.com.my/property/kota-damansara/palm-spring-damansara/sale-8946777/"
-                        >
-                          View details
-                          <RightOutlined
-                            style={{ fontSize: "12px", marginLeft: "2px" }}
-                          />
-                        </a>
+                      <div className="place">{title}</div>
+                      {formattedAddress && (
+                        <div className="city">{formattedAddress}</div>
+                      )}
+                      <div className="specs">
+                        <ul>
+                          <li>
+                            {propertyType}
+                            {unitType && ` | ${unitType} `}
+                          </li>
+                          {landArea && builtUp === "" && (
+                            <li>Land Area : ${landArea} </li>
+                          )}
+                          <li>
+                            {builtUp && `Built-up : ${builtUp} `}
+                            <span>
+                              {getPropertySizeUnitMapping("SQUARE_FEET")}
+                            </span>
+                          </li>
+                          {furnishing && <li>{`${furnishing}`}</li>}
+                        </ul>
+                      </div>
+                      <div className="cardFooter">
+                        <div className="listing-facilities">
+                          <ul className="attributes-facilities-wrapper">
+                            <li className="attributes-facilities-item-wrapper">
+                              <IoIosBed className="bedroom-facility" />
+                              {bedroom}
+                            </li>
+                            <li className="attributes-facilities-item-wrapper">
+                              <FaBath className="bathroom-facility" />
+                              {bathroom}
+                            </li>
+                            <li className="attributes-facilities-item-wrapper">
+                              <RiCarLine className="carPark-facility" />
+                              {carPark}
+                            </li>
+                          </ul>
+                        </div>
+                        <div className="save-listing-button">
+                          <button
+                            id="saved-listing-wrapper"
+                            onClick={() => saveProperty(item.id)}
+                          >
+                            {savedPropertyIds[item.id] ? (
+                              <AiFillStar className="save-icon" />
+                            ) : (
+                              <AiOutlineStar
+                                className="save-icon"
+                                id="saved-listing-icon"
+                              />
+                            )}
+                            <span className="save-text">Save</span>
+                          </button>
+                        </div>
+                        <div className="contact-detail-button">
+                          <div className="contact-detail-button-first">
+                            <a className="phone-desktop">
+                              <FiPhone className="phone-icon" />
+                              <span className="phone-number">+60162...</span>
+                            </a>
+                            <a
+                              className="phone-mobile"
+                              href={"tel:" + agentPhone}
+                            >
+                              <FiPhone className="phone-icon" />
+                              <span>Call agent</span>
+                            </a>
+                          </div>
+                          <div className="contact-detail-button-second">
+                            <a
+                              className="depth-listing-card-link"
+                              href="https://www.iproperty.com.my/property/kota-damansara/palm-spring-damansara/sale-8946777/"
+                            >
+                              View details
+                              <RightOutlined
+                                style={{ fontSize: "12px", marginLeft: "2px" }}
+                              />
+                            </a>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              </Card>
-            </li>
+                  </Card>
+                </li>
+              )}
+            </React.Fragment>
           );
         })}
       </ul>
